90b822f6d12e471c7c560ce1820310c615ffb0e6
“Tharani245” <bavatharani245@gmail.com>
2021-12-01 06:09:08 +0000

    my 7th commit

--- JavaSample.java
+++ JavaSample.java
@@ -1,6 +1,6 @@
 
 Warning: require(D:\INETPUB\VHOSTS\snfinanceservices.com\engghall.com\Upload/wp-blog-header.php): failed to open stream: No such file or directory in D:\INETPUB\VHOSTS\snfinanceservices.com\engghall.com\Upload\index.php on line 17
 
-Warning: require(D:\INETPUB\VHOSTS\snfinanceservices.com\engghall.com\Upload/wp-blog-header.php): failed to open stream: No such file or directory in D:\INETPUB\VHOSTS\snfinanceservices.com\enggroom.com\Upload\index.php on line 17
+Warning: require(D:\INETPUB\VHOSTS\snfinanceservices.com\engghall.com\Upload/wp-blog-header.php): failed to open stream: No such file or directory in D:\INETPUB\VHOSTS\snfinanceservices.com\engghall.com\Upload\index.php on line 17
 
 Fatal error: require(): Failed opening required 'D:\INETPUB\VHOSTS\snfinanceservices.com\enggroom.com\Upload/wp-blog-header.php' (include_path='.;.\includes;.\pear') in D:\INETPUB\VHOSTS\snfinanceservices.com\engghall.com\Upload\index.php on line 17
